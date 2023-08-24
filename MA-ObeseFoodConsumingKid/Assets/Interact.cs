088023287774d334f4a402ae3c31833180e5cf32
CNMarkham <96213580+ilovetapwaterr@users.noreply.github.com>
2023-08-23 20:01:20 -0400

    dsfdsf

--- MA-ObeseFoodConsumingKid/Assets/Interact.cs
+++ MA-ObeseFoodConsumingKid/Assets/Interact.cs
@@ -16,11 +16,45 @@ public class Interact : MonoBehaviour
 
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        triggerName = other.name;
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        triggerName = "";
+    }
+
     // Update is called once per frame
     void Update()
     {
+        void PlaceHeldItem()
+        {
+            Destroy(heldItem);
+            heldItemName = "";
+        }
+
         if (Input.GetKeyDown("space"))
         {
+            if (triggerName == "Recievers" && heldItemName == "BoiledHotCoffee")
+            {
+
+                PlaceHeldItem();
+
+                GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 1)").SetActive(true);
+                GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 1)/coffee").SetActive(true);
+                GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 1)/cup").SetActive(true);
+            }
+
+            if (triggerName == "Recievers" && heldItemName == "LargeBoiledHotCoffee")
+            {
+
+                PlaceHeldItem();
+
+                GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 2)").SetActive(true);
+                GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 2)/coffee1").SetActive(true);
+                GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 2)/cup1").SetActive(true);
+            }
 
             if (triggerName == "coffee")
             {
@@ -44,63 +78,24 @@ public class Interact : MonoBehaviour
                     print("Ready to Burn Your Mouth From a Boiling Slice of Coffee?");
                     stove.BoilCoffeeSlice();
                     PlaceHeldItem();
-                }
-                else
-                {
-                    print("You have Nothing to Burn Yourself With!");
                     heldItem = Instantiate(coffeePrefab, transform, false);
                     heldItem.transform.localPosition = new Vector3(0, 2, 2);
+                    Invoke("stove.CompleteCooking", 6f);
                     heldItemName = "BoiledHotCoffee";
                     stove.CleanStove();
                 }
-
-                if (triggerName == "Stove")
+                if (heldItemName == "LargeCoffeeSlice")
                 {
-                    print("Hurry Up and Use The Fridgin Stove.");
-                    if (heldItemName == "LargeCoffeeSlice")
-                    {
-                        print("Ready to Burn Your Mouth From a Boiling Slice of a Large Coffee?");
-                        stove.BoilLargeCoffeeSlice();
-                        PlaceHeldItem();
-                    }
-                    else
-                    {
-                        print("You have Nothing to Burn Yourself With!");
-                        heldItem = Instantiate(coffeePrefab, transform, false);
-                        heldItem.transform.localPosition = new Vector3(0, 2, 2);
-                        heldItemName = "LargeBoiledHotCoffee";
-                        stove.CleanStove();
-                    }
-
-                }
-                if (triggerName == "Recievers")
-                {
-                    print("I'm ready to make a dish!");
-                    if (heldItemName == "BoiledHotCoffee")
-                    {
-                        PlaceHeldItem();
-                        GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 1)").SetActive(true);
-                        GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 1)/coffee").SetActive(true);
-                        GameObject.Find("Recievers/Two Coffees/Two Coffees(Part 1)/cup").SetActive(true);
-                    }
+                    print("Ready to Burn Your Mouth From a Boiling Slice of a Large Coffee?");
+                    stove.BoilLargeCoffeeSlice();
+                    PlaceHeldItem();
+                    heldItem = Instantiate(coffeePrefab, transform, false);
+                    heldItem.transform.localPosition = new Vector3(0, 2, 2);
+                    Invoke("stove.CompleteCooking", 6f);
+                    heldItemName = "LargeBoiledHotCoffee";
+                    stove.CleanStoveLarge();
                 }
             }
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-
-            triggerName = other.name;
-        }
-        void OnTriggerExit(Collider collision)
-        {
-            triggerName = "";
-        }
-
-        void PlaceHeldItem()
-        {
-            Destroy(heldItem);
-            heldItemName = "";
         }
     }
 }
--- MA-ObeseFoodConsumingKid/Assets/Stove.cs
+++ MA-ObeseFoodConsumingKid/Assets/Stove.cs
@@ -7,6 +7,9 @@ public class Stove : MonoBehaviour
     public GameObject CoffeeSlice;
     public GameObject LargeCoffeeSlice;
     public string cookedFood = "";
+    public bool isCooking = false;
+    public ParticleSystem smoke;
+    public ParticleSystem complete;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,19 +25,40 @@ public class Stove : MonoBehaviour
 
     public void BoilCoffeeSlice()
     {
+        isCooking = true;
+        smoke.Play();
         CoffeeSlice.SetActive(true);
         cookedFood = "CoffeeSlice";
+        Invoke("CompleteCooking", 8f);
     }
 
     public void BoilLargeCoffeeSlice()
     {
+        isCooking = true;
+        smoke.Play();
         LargeCoffeeSlice.SetActive(true);
         cookedFood = "LargeCoffeeSlice";
+        Invoke("CompleteCooking", 8f);
     }
 
     public void CleanStove()
     {
         CoffeeSlice.SetActive(false);
         cookedFood = "";
+        complete.Stop();
+    }
+
+    public void CleanStoveLarge()
+    {
+        LargeCoffeeSlice.SetActive(false);
+        cookedFood = "";
+        complete.Stop();
+    }
+
+    public void CompleteCooking()
+    {
+        isCooking = false;
+        smoke.Stop();
+        complete.Play();
     }
 }
